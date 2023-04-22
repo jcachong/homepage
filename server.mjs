3a880316087ad17a26dc65987eb43405e87111ed
Justin Achong <jcachong@gmail.com>
2023-04-22 15:18:49 -0700

    Added basic support for Home and About pages.

--- server.mjs
+++ server.mjs
@@ -18,8 +18,17 @@ const MIME_TYPES = {
 };
 
 const STATIC_PATH = path.join(process.cwd(), './static');
+const PAGES_PATH = path.join(process.cwd(), './pages');
 
 const toBool = [() => true, () => false];
+
+const render = (template, params) => {
+	let view = template;
+	for(const key in params) {
+		view = view.replaceAll(`{{{${key}}}}`, params[key]);
+	}
+	return view;
+};
 
 const prepareFile = async (url) => {
 	const paths = [STATIC_PATH, url];
@@ -34,7 +43,46 @@ const prepareFile = async (url) => {
 	return { found, ext, stream };
 };
 
+const handlePageRequest = async (req, res) => {
+	let url = req.url;
+	if(url.match(/[a-zA-Z]$/g)) {
+		url += '.html'
+	}
+	const paths = [PAGES_PATH, url];
+	if(url.endsWith('/')) {
+		paths.push('index.html');
+	}
+	const filePath = path.join(...paths);
+	const pathTraversal = !filePath.startsWith(PAGES_PATH);
+	const exists = await fs.promises.access(filePath).then(...toBool);
+	const found = !pathTraversal && exists;
+	if(!found) {
+		return false;
+	}
+
+	const indexTmpl = fs.readFileSync(
+		'static/index.html').toString();
+	const page = fs.readFileSync(filePath).toString();
+	const view = render(indexTmpl, {
+		page: page,
+	});
+
+	if(view) {
+		res.writeHead(200, {
+			'Content-Type': 'text/html; charset=UTF-8'
+		});
+		res.end(view);
+		return true;
+	} else {
+		return false;
+	}
+};
+
 http.createServer(async (req, res) => {
+	const isPageRequest = await handlePageRequest(req, res);
+	if(isPageRequest) {
+		return;
+	}
 	const file = await prepareFile(req.url);
 	const statusCode = file.found ? 200 : 404;
 	const mimeType = MIME_TYPES[file.ext] || MIME_TYPES.default;
