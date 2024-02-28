1de3baf1419166819b12255d2db5aa736d19abab
Justin Achong <jcachong@gmail.com>
2024-02-28 06:33:48 -0500

    Copy over index.html, image and PDF assets. Remove unnecessary CSS and HTML Webpack bootstraps.

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
 import HtmlMinimizerPlugin from 'html-minimizer-webpack-plugin';
 import RemovePlugin from 'remove-files-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -10,6 +11,7 @@ const __dirname = path.dirname(__filename);
 
 export default {
 	entry: {
+		'index.tmp': './static/index.html',
 		'js/main.js': './static/js/main.js',
 		'css/main': './static/css/main.css',
 		'pages/about.tmp': './static/pages/about.md',
@@ -24,14 +26,25 @@ export default {
 		clean: true,
 	},
 	plugins: [
+		new CopyPlugin({
+			patterns: [
+				{ from: "static/assets", to: "assets" },
+			],
+		}),
 		new RemovePlugin({
 			after: {
 				test: [
 					{
-						folder: 'dist/pages',
+						// Removes 'index.tmp' resource asset.
+						folder: 'dist/',
 						method: (absoluteItemPath) => new RegExp(/\.tmp$/).test(absoluteItemPath),
 						recursive: true,
-					}
+					},
+					{
+						// Removes 'css/main' resource asset.
+						folder: 'dist/static/css',
+						method: (absoluteItemPath) => new RegExp(/\main$/).test(absoluteItemPath),
+					},
 				],
 			},
 		}),
@@ -39,6 +52,14 @@ export default {
 	],
 	module: {
 		rules: [
+			{
+				test: /\.html$/,
+				type: "asset/resource",
+				generator: {
+					// Removes the '.tmp' from 'index.tmp'.
+					filename: (pathData) => pathData.runtime.split('.')[0] + '.html',
+				},
+			},
 			{
 				test: /\.css$/,
 				use: [MiniCssExtractPlugin.loader, "css-loader"],
